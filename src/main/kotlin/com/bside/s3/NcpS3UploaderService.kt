e46186fafb917d7b9b631314a57195240519a288
Kimakjun <dkd7142@naver.com>
2022-08-17 05:37:59 +0900

    - 파일 업로드 api 구현

--- src/main/kotlin/com/bside/s3/NcpS3UploaderService.kt
+++ src/main/kotlin/com/bside/s3/NcpS3UploaderService.kt
@@ -4,24 +4,21 @@ import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model.CannedAccessControlList
 import com.amazonaws.services.s3.model.ObjectMetadata
 import com.amazonaws.services.s3.model.PutObjectRequest
-
 import com.bside.common.type.ErrorMessage
 import com.bside.config.s3.NcpS3Properties
 import com.bside.error.exception.NotExistException
+import com.bside.util.logger
 
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 
 @Service
 class NcpS3UploaderService(val ncpS3Client: AmazonS3, val ncpS3Properties: NcpS3Properties) {
+    private val logger by logger()
     private val FILE_EXTENSION_SEPARATOR = "."
 
-    fun upload(multipartFile: MultipartFile): String {
-        if (multipartFile.isEmpty) {
-            throw NotExistException(ErrorMessage.UPLOAD_FILE_NOT_EXIST.name, ErrorMessage.UPLOAD_FILE_NOT_EXIST.reason)
-        }
-
-        try {
+    fun uploadFile(multipartFile: MultipartFile): String? {
+        return try {
             val fileName = buildFileName(multipartFile.originalFilename!!)
             val inputStream = multipartFile.inputStream
             val objectMetadata = ObjectMetadata()
@@ -29,11 +26,19 @@ class NcpS3UploaderService(val ncpS3Client: AmazonS3, val ncpS3Properties: NcpS3
 
             ncpS3Client.putObject(PutObjectRequest(ncpS3Properties.bucketName, fileName, inputStream, objectMetadata)
                     .withCannedAcl(CannedAccessControlList.PublicRead))
-
-            return ncpS3Client.getUrl(ncpS3Properties.bucketName, fileName).toString()
+            ncpS3Client.getUrl(ncpS3Properties.bucketName, fileName).toString()
         } catch (e: Exception) {
-            throw Exception(e.message, e)
+            logger.error("file upload failed", e)
+            null
         }
+    }
+
+    fun uploadFiles(multipartFiles: List<MultipartFile>): List<String?> {
+        if (multipartFiles.isEmpty()) {
+            throw NotExistException(ErrorMessage.UPLOAD_FILE_NOT_EXIST.name, ErrorMessage.UPLOAD_FILE_NOT_EXIST.reason)
+        }
+
+        return multipartFiles.map { uploadFile(it) }
     }
 
     /**
