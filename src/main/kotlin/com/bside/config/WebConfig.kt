4a05a4336dec95489dcda39f4d5e71ba1d42fb02
snanoh <njs1324@gmail.com>
2022-08-19 21:01:55 +0900

    oauth-login  - cors 설정 변경

--- src/main/kotlin/com/bside/config/WebConfig.kt
+++ src/main/kotlin/com/bside/config/WebConfig.kt
@@ -9,8 +9,9 @@ class WebConfig: WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .exposedHeaders("X-AUTH_TOKEN")
+            //.exposedHeaders("X-AUTH_TOKEN")
             .allowCredentials(true)
-            .allowedOriginPatterns("localhost:3000","localhost:8080", "175.45.194.10","175.106.97.109")
+            .allowedMethods("*")
+            .allowedOriginPatterns("*")
     }
 }
